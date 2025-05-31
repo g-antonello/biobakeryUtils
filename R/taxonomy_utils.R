dcea4bb573f322389da1dafb068388a8117ef70d
Giacomo Antonello <giacomoantonello1@gmail.com>
2025-05-31 14:54:46 -0400

    Some taxonomy utility functions that help manipulating metaphlan 4 raw input

---
+++ R/taxonomy_utils.R
@@ -0,0 +1,91 @@
+#' Taxonomy from vector to data.frame
+#' 
+#' Expand a character vector of taxonomies into a full taxonomy table based on 
+#' the separator provided and the prefixes at each level. Tested on metaphlan4
+#' output tables
+#'
+#' @param taxonomy.chr A \code{character} vector with the taxonomy
+#' @param sep A \code{character} specifying the separator to use to split `taxonomy.chr`
+#' @param row.names Either `'short'`, `'long'`, or `'none'`. Default is 
+#' `'short'`, which returns the highest taxonomic resolution the input 
+#' `taxonomy.chr` has for that row. `'long'` returns `taxonomy.chr` as column 
+#' names. `'none'` returns a data.frame without rown ames
+#
+#' @returns A \code{data.frame}
+#' @export
+#'
+#' @examples
+
+.expand_taxonomy <- function(taxonomy.chr, sep = "|", row.names = "short"){
+  taxonomy.list <- strsplit(taxonomy.chr, sep, fixed = TRUE)
+  taxonomy.df <- as.data.frame(Reduce(rbind, taxonomy.list))
+  taxonomies_per_column <- apply(taxonomy.df, 2, function(x) unique(substr(x[!grepl("^UN*", x)], 1, 3)))
+  all_possible_taxonomy_colnames <- c(
+    d__ = "Domain",
+    k__ = "Kingdom",
+    p__ = "Phylum",
+    c__ = "Class",
+    o__ = "Order",
+    f__ = "Family",
+    g__ = "Genus",
+    s__ = "Species",
+    t__ = "SGB"
+  )
+  final_colnames <- all_possible_taxonomy_colnames[taxonomies_per_column]
+  
+  colnames(taxonomy.df) <- final_colnames
+  
+  if(row.names == "short"){
+    rownames(taxonomy.df) <- sapply(taxonomy.list, function(x) x[length(x)])
+  } 
+  
+  if(row.names == "long"){
+    rownames(taxonomy.df) <- taxonomy.chr
+  }
+  
+  if(row.names == "none"){
+    rownames(taxonomy.df) <- NULL
+  }
+  
+  return(taxonomy.df)
+}
+
+#' Transform taxonomy table into character of full taxonomies
+#'
+#' @param taxonomy.df \code{data.frame} with taxonomic levels on columns and individual taxa on rows. Taxa levels must be ordered from Kingdom/Domain to make sense, but the function does not check that
+#' @param sep \code{character} specifying the separator avoid `c("_", "-", " ", ",", "\t")` separators, because they will likely be present in the taxonomy table already or because it would conflict with saving the taxonomy table in .csv or .tsv format
+#'
+#' @returns A \code{character} of complete taxonomies separated by `sep`
+#' @export
+#'
+#' @examples
+
+.collapse_taxonomy <- function(taxonomy.df, sep = "|"){
+  
+  invalidSeps <- c("_", "-", " ", ",", "\t")
+  
+  if(sep %in% invalidSeps) {
+    warning(paste0("Please avoid this separator: ", sep))
+  }
+  
+  return(apply(as.data.frame(rowData(project4_reordered.tse)), 1, function(x) paste(x, collapse = "|")))
+  
+}
+
+.mia_rename_taxa <- function(data.tse, new_rownames){
+  #' IMPORTANT! Make sure the order of old and new row names makes sense
+  #' before running this function
+  
+  data.tse <- .reorder_taxa_with_phyloTree_labels(data.tse)
+  rownames(data.tse) <- new_rownames
+}
+
+.reorder_taxa_with_phyloTree_labels <- function(data.tse) {
+  
+  if(is.null(rowTree(data.tse))){
+    stop("No phyloTree to reorder taxa with")
+  }
+  
+  data_reordered.tse <- data.tse[rowTree(data.tse)$tip.label,]
+  return(data_reordered.tse)
+}
