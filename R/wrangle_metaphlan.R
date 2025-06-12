d4a144646074591f3db46430e53b8a39bfeaed59
Giacomo Antonello <giacomoantonello1@gmail.com>
2025-06-12 15:17:03 -0400

    moved complete_unknown_taxonomy.R into wrangle_metaphlan.R

--- R/wrangle_metaphlan.R
+++ R/wrangle_metaphlan.R
@@ -1,7 +1,61 @@
-#' wrangle a metaphlan profiles table
+#' Complete Unknown taxonomy
+#'  
+#' This is a utility function for the `metaphlan_wrangle function`, which needs
+#' it in case it finds taxa that are unique at higher taxonomies levels than the 
+#' one requested (e.g. a Genus without any species mapped to it. maybe it is still 
+#' a valuable genus, so it has to be repeated with the same name at the taxonomic level below).
+#' This is also valuable because another approach would discard it, and the total
+#' relative abundances estimated would not add up.
+#' 
+#' @param x \code{character},  the taxonomy as it appears in the first column of a metaphlan table
+#' @param tax_lvl_int \code{character} the level at which the user wants the taxonomies aggregated. The default goes all the way to the SGB level (`8`). Species level is `7`, Genus is `6` and so on.
 #'
-#' @param mpa a Metaphlan matrix as a data frame
-#' @param taxonomic_lvl \code{character} indicating the taxonomic level wanted. Default is `Species`
+#' @return \code{character}, the same as the input vector, with the lowest taxonomy repeated down to the `tax_lvl_int` chosen
+#' @importFrom tidyr separate_wider_delim
+#' @export
+#'
+#' @examples
+#' 
+#' example_taxonomies <- c("k__Bacteria|p__Firmicutes|c__CFGB1347", "k__Bacteria|p__Actinobacteria", "k__Bacteria|p__Firmicutes|c__Clostridia|o__Eubacteriales|f__Eubacteriales_Family_XIII_Incertae_Sedis|g__Lentihominibacter|s__Lentihominibacter_faecis")
+#' 
+#' # complete taxonomies to species level (level of taxonomic depth)
+#' complete_unknown_taxonomy(example_taxonomies, tax_lvl_int = 7)
+#' 
+#' 
+
+complete_unknown_taxonomy <- function(x, tax_lvl_int = 8){
+  
+  #input checks 
+  if (!is.character(x)) {
+    stop("Input 'x' must be a character vector.")
+  }
+  
+  if (!is.numeric(tax_lvl_int) || tax_lvl_int < 1 || tax_lvl_int > 8 || tax_lvl_int != round(tax_lvl_int)) {
+    stop("'tax_lvl_int' must be an integer between 1 and 8.")
+  }
+  
+  tmp <- sapply(strsplit(x, "\\|"), function(l) l[length(l)])
+  possible_levels <- c("k__","p__","c__","o__", "f__", "g__", "s__", "t__")[1:tax_lvl_int]
+  latest_taxonomy <- which(grepl(substr(tmp, 1, 3), possible_levels))
+  if(length(latest_taxonomy) == 0){
+    latest_taxonomy <- 0
+    return(paste(rep(tmp, tax_lvl_int), collapse = "|"))
+  } else{
+    return(paste(c(x, rep(tmp, tax_lvl_int - latest_taxonomy)), collapse = "|")) 
+  }
+}
+
+
+#' wrangle a metaphlan profiles table
+#' 
+#' Works really similarly to mia::importMetaphlan, the difference being that 
+#' this function starts from data already loaded in R. Also, it does not 
+#' handel ncbi_tax_id so well
+#' 
+#' @param mpa a Metaphlan matrix as a data frame as comes out after 
+#' `merge_metaphlan.py`
+#' @param taxonomic_lvl \code{character} indicating the taxonomic level wanted. 
+#' Default is `Species`
 #'
 #' @return a \code{list containing both `profiles` and `taxonomies`}
 #' @importFrom purrr transpose reduce
@@ -11,19 +65,20 @@
 #' @examples
 #' 
 #' data("metaphlanTestData")
-#' wrangled_metaphlans <- sapply(c("Kingdom", "Class","Species"),
-#' function(taxLvl)
-#' wrangle_metaphlan(metaphlanTestData, taxonomic_lvl = taxLvl), 
+#' wrangled_metaphlans <- sapply(c("Kingdom", "Class","Species"), 
+#'   function(taxLvl) {
+#'   wrangle_metaphlan(metaphlanTestData, taxonomic_lvl = taxLvl)
+#'   }, 
 #' USE.NAMES = TRUE, simplify = FALSE)
 
 wrangle_metaphlan <- function(mpa, taxonomic_lvl = "Species"){
   
-  taxonomies <- c("Kingdom", "Phylum", "Class", "Order", "Family", "Genus", "Species", "SGB")
+  # the package imports all_taxonomy_levels, a named character vector
   
   # for each row, find to which depth of taxonomy it arrives (as integers)
   # the +1 at the end ensures that kingdom is level 1 and not 0
   tax_lengths <- lengths(regmatches(mpa$clade_name, gregexpr("\\|", mpa$clade_name))) + 1
-  tax_lvl_int <- which(tolower(taxonomic_lvl) == tolower(taxonomies))
+  tax_lvl_int <- which(tolower(taxonomic_lvl) == tolower(all_taxonomy_levels))
 
   selection_vec <- tax_lengths == tax_lvl_int
   
@@ -77,8 +132,8 @@ wrangle_metaphlan <- function(mpa, taxonomic_lvl = "Species"){
     ) |>
       unlist()
     
-    #' replace original names with those that were gradually added going up the
-    #' taxonomic tree
+    # replace original names with those that were gradually added going up the
+    # taxonomic tree
     
     mpa_refined$clade_name <- sapply(mpa_refined$clade_name, function(x) 
       ifelse(
