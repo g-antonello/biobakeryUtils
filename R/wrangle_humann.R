bced13c0d889f3c3c678a096ad637840efa2a2ee
Giacomo Antonello <giacomoantonello1@gmail.com>
2025-10-29 17:08:25 -0400

    Now HUMAnN KO info get split in a more general way. this avoids splitting descriptions in case EC numbers are not present

--- R/wrangle_humann.R
+++ R/wrangle_humann.R
@@ -1,17 +1,14 @@
 wrangle_humann <- function(input_table, input_type = "ko"){
   
   if(input_type == "ko"){
-  description.list <- strsplit(input_table[[1]], "\\ ") 
+  featureName <- input_table[[1]]
   
   # extract KO information
-  ko_only <- gsub(":", "", sapply(description.list, "[", 1), fixed = TRUE)
+  ko_only <- sapply(strsplit(featureName, ": ", fixed = TRUE), "[", 1)
   # extract EC information
-  EC_only <- gsub("\\[|\\]", "", sapply(description.list, function(x) x[length(x)])) 
+  EC_only <- tryCatch(sapply(strsplit(featureName, "[", fixed = TRUE), function(x) ifelse(length(x) > 1, gsub("]", "", x[length(x)]), NA)), error = function(e) return(NA))
   # extract enzyme name information
-  enz_name <- sapply(description.list, function(x) 
-      tryCatch(x[2:(length(x) - 1)], 
-               error = function(e) return(x))) |> 
-    sapply(paste, collapse = " ")
+  enz_name <- tryCatch(sapply(strsplit("K00383: glutathione reductase (NADPH) [EC:1.8.1.7]", "\\:\\ |\\ \\["), "[", 2), error = function(e) return(NA))
   
   # build a data.frame with these data
   description.df <- data.frame(
